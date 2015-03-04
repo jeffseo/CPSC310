5eee014cb63ae1cef11c152dbbf91f36aa5ac9d7
Sainan <sainanliu7@gmail.com>
2015-03-04 13:06:17 -0800

    Rolled back the scaffold User and it's db:migration

--- VanVendorDiary/db/schema.rb
+++ VanVendorDiary/db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302215321) do
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
