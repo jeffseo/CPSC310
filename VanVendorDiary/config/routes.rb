045393ab274a9323db4ea960e7294551b15e52ab
Sainan <sainanliu7@gmail.com>
2015-03-04 14:35:51 -0800

    Route /signup to new function in Users.

--- VanVendorDiary/config/routes.rb
+++ VanVendorDiary/config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'welcome/index'
+  root 'welcome#index'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
