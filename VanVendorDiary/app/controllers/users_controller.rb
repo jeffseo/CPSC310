3d1f2b269da805ff5a980a3d6646a02712fc1c73
Sainan <sainanliu7@gmail.com>
2015-03-05 13:07:44 -0800

    Created first user profile page with avatar.

--- VanVendorDiary/app/controllers/users_controller.rb
+++ VanVendorDiary/app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  # Created so that /users view can work
+  def index
+    @user = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+  
+  def create
+    render plain: params[:user].inspect
+  end
+  
   def new
+    @user = User.new
   end
 end
--- VanVendorDiary/test/controllers/welcome_controller_test.rb
+++ VanVendorDiary/test/controllers/welcome_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Vancouver Vendor Diary"
+  end
+  
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "Welcome | #{@base_title}"
   end
 
 end
