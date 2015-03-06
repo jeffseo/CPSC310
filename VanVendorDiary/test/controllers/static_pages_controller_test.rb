41702e3c95e787df79404b1efd6c5ee2886f3a59
Sainan <sainanliu7@gmail.com>
2015-03-06 14:32:21 -0800

    Destroied welcome page.

--- VanVendorDiary/test/controllers/static_pages_controller_test.rb
+++ VanVendorDiary/test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Vancouver Vendor Diary"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Vancouver Vendor Diary"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Vancouver Vendor Diary"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Vancouver Vendor Diary"
+    assert_select "title", "About | #{@base_title}"
   end
 end
