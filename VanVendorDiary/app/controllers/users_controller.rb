6f3b6cec0bfd69c8e45e80c2bc5d96babc6e5604
Sainan <sainanliu7@gmail.com>
2015-03-05 16:22:06 -0800

    Finish user signup, added a few style change.

--- VanVendorDiary/app/controllers/users_controller.rb
+++ VanVendorDiary/app/controllers/users_controller.rb
@@ -9,11 +9,23 @@ class UsersController < ApplicationController
     #debugger
   end
   
-  def create
-    render plain: params[:user].inspect
-  end
-  
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # equivalent to redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
