1511a4e35b2f7eff9bb0b44a01bb8a4fc6aa75b3
Sainan <sainanliu7@gmail.com>
2015-03-05 17:09:38 -0800

    Added puma as the production server.

--- VanVendorDiary/app/controllers/users_controller.rb
+++ VanVendorDiary/app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to your Vancouver Vendor Diary!"
       redirect_to @user # equivalent to redirect_to user_url(@user)
     else
       render 'new'
