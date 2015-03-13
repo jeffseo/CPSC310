2bb4919d9493ab17035f1c6a6113cc51709607d2
ywang687 <n0d0b@dhcp-128-189-90-26.ubcsecure.wireless.ubc.ca>
2015-03-13 01:14:21 -0700

    finally works perfectly

--- VanVendorDiary/config/initializers/omniauth.rb
+++ VanVendorDiary/config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '723646061101119', 'ec2bd3a293728ead5040c7a954d9f69e'
+  provider :facebook, '723646061101119', 'ec2bd3a293728ead5040c7a954d9f69e',
+  		:scope => 'email,user_birthday,read_stream', :display => 'popup'
 end
