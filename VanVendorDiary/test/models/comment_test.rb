c6afd80b03e3bd98148a183b4c38c84f51225dab
Sainan <sainanliu7@gmail.com>
2015-03-17 22:59:07 -0700

    Created comment model. belongs to user.

---
+++ VanVendorDiary/test/models/comment_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @comment = Comment.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @comment.valid?
+  end
+
+  test "user id should be present" do
+    @comment.user_id = nil
+    assert_not @comment.valid?
+  end
+  
+  test "content should be present " do
+    @comment.content = "   "
+    assert_not @comment.valid?
+  end
+
+  test "content should be at most 200 characters" do
+    @comment.content = "a" * 201
+    assert_not @comment.valid?
+  end
+end
