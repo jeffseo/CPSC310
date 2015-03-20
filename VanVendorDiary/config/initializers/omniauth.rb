52bd6f837dfe9370026dbc54d80efa94dff09ccf
ywang687 <n0d0b@dhcp-128-189-90-26.ubcsecure.wireless.ubc.ca>
2015-03-20 13:11:07 -0700

    added separate localhost and production

--- VanVendorDiary/config/initializers/omniauth.rb
+++ VanVendorDiary/config/initializers/omniauth.rb
@@ -1,6 +1,13 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '723646061101119', 'ec2bd3a293728ead5040c7a954d9f69e',
-  		:scope => 'email,user_birthday,read_stream', :display => 'popup'
+  if Rails.env.development?
+    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    provider :facebook, '723646061101119', 'ec2bd3a293728ead5040c7a954d9f69e',
+      		:scope => 'email,user_birthday,read_stream', :display => 'popup'
+
+  else
+    provider :facebook, '832584233475739', '0ccc79e1773425635c9a0b0db748f973',
+      		:scope => 'email,user_birthday,read_stream', :display => 'popup'
+  end
 end
