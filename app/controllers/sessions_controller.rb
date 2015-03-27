91bfadf306cc81dbdb2a1c03da076ed8f952f8b4
ywang687 <n0d0b@dhcp-128-189-90-26.ubcsecure.wireless.ubc.ca>
2015-03-27 14:15:08 -0700

    first

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  #omniauth
+  def create2
+    auth = request.env["omniauth.auth"]
+    omniauthuser = Omniauthuser.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Omniauthuser.create_with_omniauth(auth)
+    omniauthuser = Omniauthuser.omniauth(env['omniauth.auth'])
+    session[:user_id] = omniauthuser.id
+    log_in omniauthuser
+    redirect_to root_url, :notice=> "Signed in! Hello " + omniauthuser.name + "!"
+  end
+
+  def failure  
+    redirect_to root_url, alert: "Authentication failed, please try again."  
+  end  
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    log_out
+    redirect_to root_url
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  # Created so that /users view can work
+  def index
+    @user = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to your Vancouver Vendor Diary!"
+      redirect_to @user # equivalent to redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
