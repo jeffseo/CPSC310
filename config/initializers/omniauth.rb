1090921553ad47cfece67097d1aa1749ed912dce
ywang687 <n0d0b@dhcp-128-189-90-26.ubcsecure.wireless.ubc.ca>
2015-03-28 11:41:38 -0700

    changed ssl

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,9 +11,9 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development?
     OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-    provider :facebook, '723646061101119', 'ec2bd3a293728ead5040c7a954d9f69e'
+    provider :facebook, '723646061101119', 'ec2bd3a293728ead5040c7a954d9f69e', {:client_options => {:ssl => {:verify => false}}}
+
   else
-  	OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-    provider :facebook, '832584233475739', '0ccc79e1773425635c9a0b0db748f973'
+    provider :facebook, '832584233475739', '0ccc79e1773425635c9a0b0db748f973', {:client_options => {:ssl => {:verify => false}}}
   end
 end
