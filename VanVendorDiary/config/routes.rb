09eecc2f4ce6491720f103f9f68c7391950c886a
Sainan <sainanliu7@gmail.com>
2015-04-01 16:53:53 -0700

    Fixed merge

--- VanVendorDiary/config/routes.rb
+++ VanVendorDiary/config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   get 'vendors/index'
   get 'vendors/autoimport' => 'vendors#autoimport'
+  #get 'auth/:provider/callback', to: 'sessions#facebookcreate'
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
